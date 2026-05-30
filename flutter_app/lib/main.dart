e9456b7b533aa55e96d298a48bab65e17d6c59bb
M Arslan Ahmad <developer.arslanahmad@gmail.com>
2026-05-30 13:12:14 +0500

    Fix: Add INTERNET permission to AndroidManifest for Render cloud API access

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 import 'package:fl_chart/fl_chart.dart';
 
 
-const String BASE_URL = 'https://reviewlens-d401.onrender.com'; // render cloud 
+const String BASE_URL = 'https://reviewlens-d401.onrender.com'; // render cloud
 // const String BASE_URL = 'http://192.168.100.7:8000'; real phone manual
 // const String BASE_URL = 'http://10.0.2.2:8000'; // emulator
 // const String BASE_URL = 'http://YOUR_LAPTOP_IP:8000'; // real phone
